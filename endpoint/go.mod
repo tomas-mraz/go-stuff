fa138339a1d94fa6de295aa7086177ae04e22470
Tomas Mraz <tomas.mraz@zona64.cz>
2023-10-12 13:48:11 +0200

    showing logs page per context

--- endpoint/go.mod
+++ endpoint/go.mod
@@ -1,3 +1,5 @@
 module endpoint
 
-go 1.19
+go 1.20
+
+require github.com/gorilla/mux v1.8.0
--- endpoint/main.go
+++ endpoint/main.go
@@ -3,81 +3,145 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"io"
 	"log/slog"
 	"net/http"
+	"os"
 	"sync"
 	"time"
 )
 
+type endpoint struct {
+	lastUpdate time.Time
+	pointer    int
+	requests   [maxRequests]string
+}
+
+const maxRequests = 30
+
 var (
-	requests    []string
-	requestsMu  sync.Mutex
-	port        = "8080"
-	maxRequests = 30
-	htmlPage    = "<html><head><title>BlackHole endpoint</title></head>" +
-		"<body><h1>Tady žijí dva endpointy</h1><ul>" +
-		"<li><b>/ping</b> ... který přijímá requesty</li>" +
-		"<li><a href=\"/log\"><b>/log</b></a> ... který zobrazuje posledních 30 přijatých requestů</li>" +
-		"</ul></body></html>"
+	endpoints  map[string]*endpoint
+	requestsMu sync.Mutex
+	htmlPage   = "<html><head><title>BlackHole endpoint</title></head>" +
+		"<body><h1>Tady žijí testovací endpointy</h1><ul>" +
+		"<li><b>/</b> ... tahle stránka</li>" +
+		"<li><b>/log</b> ... již neukazuje souhrnné logy (funguje jako přijímací endpoint viz /* níže)</li>" +
+		"<li><b>/*</b> ... jakýkoli název kontextu přijímá requesty a vrací 200:{\"message\":\"Status OK\"}</li>" +
+		"<li><a href=\"/*/log\">/*<b>/log</b></a> ... <KONTEXT>/log zobrazuje posledních 30 přijatých requestů pro daný kontext (poslední je nahoře)</li>" +
+		"</ul>" +
+		"Příklady:<br>" +
+		"ferda.zona64.cz:8080<b>/magda</b> ... bude mít logy na /magda/log<br>" +
+		"ferda.zona64.cz:8080<b>/magda/123</b> ... bude mít logy na /magda/123/log<br>" +
+		"</body></html>"
 )
 
-func pingHandler(w http.ResponseWriter, r *http.Request) {
+func pingHandler(writer http.ResponseWriter, request *http.Request) {
+	// skip requests to favicon
+	if request.URL.Path == "/favicon.ico" {
+		return
+	}
+
 	requestsMu.Lock()
 	defer requestsMu.Unlock()
+
+	params := mux.Vars(request)
+	name := params["name"]
+
 	// logging body
-	buf, err := io.ReadAll(r.Body)
+	buf, err := io.ReadAll(request.Body)
 	if err != nil {
 		slog.Error("Body read Error: " + err.Error())
 		return
 	}
 	formattedBody := ""
 	if len(buf) != 0 {
+		// body je na nové řádce
 		formattedBody = "\n" + string(buf)
 	}
 
-	requests = append(requests, time.Now().Format(time.RFC3339)+" - "+r.Method+" "+r.URL.Path+formattedBody+"\n")
+	e, ok := endpoints[name]
+	if !ok {
+		f := endpoint{time.Now(), 0, [maxRequests]string{}}
+		endpoints[name] = &f
+		e = &f
+	}
+	e.lastUpdate = time.Now()
+	e.requests[e.pointer] = time.Now().Format(time.RFC3339) + " - " + request.Method + " " + request.URL.Path + formattedBody + "\n"
 
-	if len(requests) > maxRequests {
-		// Remove the oldest request to keep the list within the limit
-		requests = requests[len(requests)-maxRequests:]
+	e.pointer++
+	if e.pointer >= maxRequests {
+		e.pointer = 0
 	}
 
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	writer.Header().Set("Content-Type", "application/json")
 	resp := make(map[string]string)
 	resp["message"] = "Status OK"
 	jsonResp, err := json.Marshal(resp)
 	if err != nil {
 		slog.Error("Error happened in JSON marshal: " + err.Error())
 	}
-	w.Write(jsonResp)
+	_, err = writer.Write(jsonResp)
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
 	return
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte(htmlPage))
-	if err != nil {
-		slog.Error("writing homepage error: " + err.Error())
-		return
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+	slog.Debug(r.URL.Path)
+	if r.URL.Path == "/" {
+		_, err := w.Write([]byte(htmlPage))
+		if err != nil {
+			slog.Error("writing homepage error: " + err.Error())
+			return
+		}
 	}
 }
 
-func logHandler(w http.ResponseWriter, r *http.Request) {
+func logPageHandler(writer http.ResponseWriter, request *http.Request) {
 	requestsMu.Lock()
 	defer requestsMu.Unlock()
 
+	params := mux.Vars(request)
+	name := params["name"]
+
+	e := endpoints[name]
+	if e == nil {
+		_, err := fmt.Fprintln(writer, "tento endpoint jeste nikdo nezavolal")
+		if err != nil {
+			slog.Error(err.Error())
+		}
+		return
+	}
+
 	// Print requests in reverse order
-	for i := len(requests) - 1; i >= 0; i-- {
-		fmt.Fprintln(w, requests[i])
+	for i := 0; i < maxRequests; i++ {
+		a := e.pointer - i - 1 // pointer target to index of next item (not the last one)
+		if a < 0 {
+			a = maxRequests + a
+		}
+		_, err := fmt.Fprintln(writer, e.requests[a])
+		if err != nil {
+			slog.Error(err.Error())
+			return
+		}
 	}
 }
 
 func main() {
+	endpoints = make(map[string]*endpoint)
+	port := os.Args[1]
 	slog.Info("Start listening on port " + port)
-	http.Handle("/", http.HandlerFunc(homeHandler))
-	http.Handle("/ping", http.HandlerFunc(pingHandler))
-	http.Handle("/log", http.HandlerFunc(logHandler))
+
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/{name:.+}/log", logPageHandler).Methods("GET")
+	rtr.HandleFunc("/{name:.+}", pingHandler).Methods("GET")
+	rtr.HandleFunc("/", homePageHandler).Methods("GET")
+	http.Handle("/", rtr)
+
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		slog.Error(err.Error())
