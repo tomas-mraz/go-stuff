f44dccaefee3a0ec63d5aa19630a99a450a7b86a
Tomas Mraz <tomas.mraz@zona64.cz>
2023-10-23 21:15:24 +0200

    feat(download): cancellation of download process on the background

--- downloadFile/main.go
+++ downloadFile/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/dustin/go-humanize"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"time"
@@ -15,23 +15,32 @@ type progressReader struct {
 	size     int64
 	position int64
 	start    int64
+	context  context.Context
 }
 
 func main() {
 	tempFile, _ := os.OpenFile("tmp.zip", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	defer tempFile.Close()
 
-	rrr := &progressReader{}
-	go downloadFile(rrr, "https://cz.releases.ubuntu.com/releases/23.10.1/ubuntu-23.10.1-desktop-amd64.iso", tempFile)
+	ctx, cancel := context.WithCancel(context.Background())
+	aaa := &progressReader{context: ctx}
+	go downloadFile(aaa, ctx, "https://releases.ubuntu.com/22.04.3/ubuntu-22.04.3-live-server-amd64.iso", tempFile)
 
-	time.Sleep(10 * time.Second)
-	rrr.Output()
-	time.Sleep(15 * time.Second)
-	rrr.Output()
-	time.Sleep(20 * time.Second)
-	rrr.Output()
-	time.Sleep(120 * time.Second)
-	rrr.Output()
+	time.Sleep(5 * time.Second)
+	aaa.Output()
+	time.Sleep(5 * time.Second)
+	aaa.Output()
+
+	cancel()
+	ctx2, cancel2 := context.WithCancel(context.Background())
+	bbb := &progressReader{context: ctx2}
+	go downloadFile(bbb, ctx2, "https://releases.ubuntu.com/23.10.1/ubuntu-23.10.1-desktop-amd64.iso", tempFile)
+	defer cancel2()
+
+	time.Sleep(5 * time.Second)
+	bbb.Output()
+	time.Sleep(5 * time.Second)
+	bbb.Output()
 	fmt.Println("end")
 }
 
@@ -70,24 +79,36 @@ func (pr *progressReader) Progress() string {
 	return fmt.Sprintf("%3.0f%%", percentage)
 }
 
-func downloadFile(pr *progressReader, url string, tempFile *os.File) {
+func downloadFile(pr *progressReader, ctx context.Context, url string, tempFile *os.File) {
 	req, _ := http.NewRequest("GET", url, nil)
 	resp, _ := http.DefaultClient.Do(req)
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println("error during closing connection")
+		}
+	}(resp.Body)
 
 	pr.reader = resp.Body
 	pr.size = resp.ContentLength
+	pr.position = 0
 	pr.start = time.Now().UnixMilli()
+
 	_, err := io.Copy(tempFile, pr)
 	if err != nil {
-		log.Fatal("error during downloading")
+		fmt.Println("error or cancellation during downloading")
 	}
 }
 
 func (pr *progressReader) Read(p []byte) (int, error) {
-	n, err := pr.reader.Read(p)
-	if err == nil {
-		pr.position += int64(n)
+	select {
+	case <-pr.context.Done():
+		return 0, pr.context.Err()
+	default:
+		n, err := pr.reader.Read(p)
+		if err == nil {
+			pr.position += int64(n)
+		}
+		return n, err
 	}
-	return n, err
 }
