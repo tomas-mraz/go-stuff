fd7e92b486941c2019d3237810410d19786e2bef
Tomáš Mráz <tomas.mraz@zona64.cz>
2024-07-26 21:31:41 +0200

    GGG

--- yaml/go.mod
+++ yaml/go.mod
@@ -2,6 +2,4 @@ module example.com/yaml
 
 go 1.22
 
-require (
-	gopkg.in/yaml.v3 v3.0.1
-)
+require gopkg.in/yaml.v3 v3.0.1
--- yaml/main.go
+++ yaml/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"gopkg.in/yaml.v3"
+	"log/slog"
+	"os"
 )
 
 type Config struct {
@@ -21,18 +23,43 @@ b:
 `
 
 func main() {
-	fmt.Println("ahoj")
-	t := Config{}
 
-	err := yaml.Unmarshal([]byte(data), &t)
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-	fmt.Printf("--- t:\n%v\n\n", t)
+	// Create config structure
+	config := &Config{}
 
-	d, err := yaml.Marshal(&t)
+	// Open config file
+	file, err := os.Open("aaa.yaml")
 	if err != nil {
-		log.Fatalf("error: %v", err)
+		slog.Error("adsdas " + err.Error())
 	}
+	defer file.Close()
+
+	// Init new YAML decode
+	d := yaml.NewDecoder(file)
+
+	// Start YAML decoding from file
+	if err2 := d.Decode(&config); err2 != nil {
+		slog.Error("asdasd " + err2.Error())
+	}
+
+	fmt.Println(config.A)
+
+	/*
+		fmt.Println("ahoj")
+		t := Config{}
+
+		err := yaml.Unmarshal([]byte(data), &t)
+		if err != nil {
+			slog.Error("error: ")
+		}
+		fmt.Printf("--- t:\n%v\n\n", t)
+
+		d :=
+		d, err2 = yaml.Marshal(&t)
+		if err2 != nil {
+			slog.Error("error2: ")
+		}
+	*/
+
 	fmt.Printf("--- t dump:\n%s\n\n", string(d))
 }
